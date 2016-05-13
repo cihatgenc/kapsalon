e7dc1d17b1f009b97f2c9c5bd81dc02b77e8adeb
Cihat Genc <cgenc@schubergphilis.com>
2016-05-13 16:25:21 +0200

    Initial Commit

---
+++ Kapsalon.Service/KapsalonService.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsalon.Service
+{
+    public partial class KapsalonService : ServiceBase
+    {
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+        public KapsalonService()
+        {
+            _logger.Info("Service starting");
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        /// <summary>
+        /// Starts the service as console application when running interactively.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public void StartConsole(string[] args)
+        {
+            OnStart(args);
+        }
+        /// <summary>
+        /// Stops the service as console application when running interactively.
+        /// </summary>
+        public void StopConsole()
+        {
+            OnStop();
+        }
+    }
+}
---
+++ Kapsalon.Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace Kapsalon.Service
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var service = new KapsalonService();
+            if (Environment.UserInteractive)
+            {
+                service.StartConsole(args);
+                Console.WriteLine("Press any key to stop program");
+                Console.Read();
+                service.StopConsole();
+            }
+            else
+            {
+                ServiceBase.Run(service);
+            }
+        }
+    }
+}
